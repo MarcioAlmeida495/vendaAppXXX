cd4c84975ec1419abc374237bad8059dcb316d46
MarcioAlmeida495 <39245068+MarcioAlmeida495@users.noreply.github.com>
2024-10-02 15:25:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port  = 8080;
+const port  = 80;
 const cors = require('cors');
 app.use(cors());
 console.log("rodando em : " + port)
